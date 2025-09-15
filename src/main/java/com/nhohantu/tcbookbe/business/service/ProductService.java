44fc140423bebc42326092779e286828966be3ae
Sown13 <sonnh.buh@gmail.com>
2025-09-16 00:58:37 +0700

    sonnh/feat: clean

--- src/main/java/com/nhohantu/tcbookbe/business/service/ProductService.java
+++ src/main/java/com/nhohantu/tcbookbe/business/service/ProductService.java
@@ -15,9 +15,7 @@ import lombok.AllArgsConstructor;
 import lombok.CustomLog;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
