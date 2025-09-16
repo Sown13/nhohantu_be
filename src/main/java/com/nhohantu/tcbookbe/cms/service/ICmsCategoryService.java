256c35767fd60b8b21c482993b6ecf377dec0dd4
hieu-nguyen <hieunguyen2005q@gmail.com>
2025-09-16 18:02:44 +0700

    feat: Moved CmsCategoryService.java out of implement directory to comply with new project structure

--- src/main/java/com/nhohantu/tcbookbe/cms/service/ICmsCategoryService.java
+++
@@ -1,8 +0,0 @@
-package com.nhohantu.tcbookbe.cms.service;
-
-import com.nhohantu.tcbookbe.common.repository.BaseCategoryRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICmsCategoryService extends BaseCategoryRepository {
-}
