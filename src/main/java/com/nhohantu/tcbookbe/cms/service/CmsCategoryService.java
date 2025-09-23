29348869bed63f29dfc6379a0d8ad014d7d990d9
hieu-nguyen <hieunguyen2005q@gmail.com>
2025-09-23 13:18:12 +0700

    done api get categories level 3

--- src/main/java/com/nhohantu/tcbookbe/cms/service/CmsCategoryService.java
+++ src/main/java/com/nhohantu/tcbookbe/cms/service/CmsCategoryService.java
@@ -21,6 +21,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Log4j2
@@ -102,6 +103,22 @@ public class CmsCategoryService {
                     StatusCodeEnum.ERRORCODE4000);
         }
     }
-//    public ResponseEntity<ResponseDTO<List<CmsCreateCategoryResponse>>>
+    public ResponseEntity<ResponseDTO<List<CmsCreateCategoryResponse>>> findAllCategoryLevel3(){
+        try{
+            List<CategoryModel> categories = categoryService.findByCategoryLevel(3);
+
+            List<CmsCreateCategoryResponse> responseList = categories.stream()
+                    .map(category-> mapper.map(category, CmsCreateCategoryResponse.class))
+                    .collect(Collectors.toList());
+
+            return ResponseBuilder.okResponse("Lấy danh sách danh mục cấp 3 thành công", responseList, StatusCodeEnum.SUCCESS2000);
+
+        }catch (IllegalArgumentException e){
+            log.error(e.getMessage());
+            return ResponseBuilder.badRequestResponse(e.getMessage(),
+                    StatusCodeEnum.ERRORCODE4000);
+        }
+
+    }
 }
 
