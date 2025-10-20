3b83ea99ea5720145e1b91ac2281a2ab5c51ce36
Quanghuy612 <huypro0612@gmail.com>
2025-10-20 08:17:47 +0700

    20/10
    - Update best sellter product, login api

--- src/main/java/com/nhohantu/tcbookbe/business/repository/IProductRepository.java
+++ src/main/java/com/nhohantu/tcbookbe/business/repository/IProductRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -34,4 +35,6 @@ public interface IProductRepository extends BaseProductRepository {
     );
 
     Optional<ProductModel> findBySlug(String slug);
+
+    List<ProductModel> findBestProductByOrderByRatingDesc();
 }
