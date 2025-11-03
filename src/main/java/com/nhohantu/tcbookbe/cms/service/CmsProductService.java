939b4ee95728608561049d4cc2894d1e04eb2774
Le Hong Hai <hlehong163@gmail.com>
2025-11-04 04:39:17 +0700

    test create

--- src/main/java/com/nhohantu/tcbookbe/cms/service/CmsProductService.java
+++ src/main/java/com/nhohantu/tcbookbe/cms/service/CmsProductService.java
@@ -103,6 +103,8 @@ public class CmsProductService {
             product.setTags(tags);
         }
 
+        product.setSlug("");
+
         try {
             ProductModel result = productRepository.save(product);
             CmsCreateProductResponse response = mapper.map(result, CmsCreateProductResponse.class);
