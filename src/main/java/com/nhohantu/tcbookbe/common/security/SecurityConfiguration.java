0569c3a007d27bfb8dcf00a34c2f4f846da334b2
Le Hong Hai <hlehong163@gmail.com>
2025-12-23 16:50:33 +0700

    done all

--- src/main/java/com/nhohantu/tcbookbe/common/security/SecurityConfiguration.java
+++ src/main/java/com/nhohantu/tcbookbe/common/security/SecurityConfiguration.java
@@ -16,6 +16,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.http.HttpMethod.OPTIONS;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
